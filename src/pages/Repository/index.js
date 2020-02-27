b10b86b46bd4efb34580cd32b1076454af59f316
LucasE2996 <moeda.60@gmail.com>
2020-02-26 23:38:46 -0300

    carregando dados da api

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
 
 export default function Repository({ match }) {
+    const [repository, setRepository] = useState([]);
+    const [issues, setIssues] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function getRepositoryData(repName) {
+            return await Promise.all([
+                api.get(`/repos/${repName}`),
+                api.get(`/repos/${repName}/issues`, {
+                    params: {
+                        state: 'open',
+                        per_page: 5,
+                    }
+                }),
+            ]);
+        }
+
+        const repName = decodeURIComponent(match.params.name);
+
+        getRepositoryData(repName).then(values => {
+            const [repository, issues] = values;
+
+            setRepository(repository.data);
+            setIssues(issues.data);
+            setLoading(false);
+        });
+    }, []);
+
     return <h1>Repository: {decodeURIComponent(match.params.name)}</h1>;
 }
