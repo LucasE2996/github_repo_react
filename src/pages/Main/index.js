d709569657e3b9ad87c3d9775b1d8a3483024735
LucasE2996 <moeda.60@gmail.com>
2020-02-26 22:54:22 -0300

    utilizando localstorage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
 import api from '../../services/api';
@@ -9,6 +9,23 @@ export default function Main() {
     const [newRepo, setNewRepo] = useState('');
     const [repositories, setRepositories] = useState([]);
     const [loading, setLoading] = useState(false);
+
+    /**
+     * equivalent to componentDidMount, componentDidUpdate and componentWillUnmount
+     * combined together in class component
+     */
+    useEffect(() => {
+        const repositories = JSON.parse(localStorage.getItem('repositories'));
+
+        setRepositories(repositories);
+    }, []);
+
+    useEffect(() => {
+        // load data from local storage
+        localStorage.setItem('repositories', JSON.stringify(repositories));
+    }, [repositories]);
+
+
 
     function handleInputChange(e) {
         setNewRepo(e.target.value);
